d431b2173365d42eac5cd948879ae81076f4b2d1
benitesjose149-maker <benitesjose149@gmail.com>
2026-05-25 15:31:42 -0500

    fix: authService public

--- frontend/src/app/components/layout/layout.ts
+++ frontend/src/app/components/layout/layout.ts
@@ -12,7 +12,7 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LayoutComponent {
 
-  private authService = inject(AuthService);
+  public authService = inject(AuthService);
   private router = inject(Router);
 
   isSidebarCollapsed = signal(false);
